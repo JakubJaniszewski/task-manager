bc166c78066a3ec246c210cc8c1bf3364de9b7c1
JakubJaniszewski <cwanykuba@gmail.com>
2017-11-17 03:17:08 +0100

    Implement javascript drag'n'drop functionality

---
+++ src/main/resources/static/js/drag_drop.js
@@ -0,0 +1,17 @@
+function _(id) {
+    return document.getElementById(id);
+}
+
+var droppedIn = false;
+
+function drag_start(event) {
+    event.dataTransfer.dropEffect = "move";
+    event.dataTransfer.setData("text", event.target.getAttribute('id') );
+}
+
+function drag_drop(event) {
+    event.preventDefault(); /* Prevent undesirable default behavior while dropping */
+    var elem_id = event.dataTransfer.getData("text");
+    event.target.appendChild( _(elem_id) );
+    droppedIn = true;
+}
